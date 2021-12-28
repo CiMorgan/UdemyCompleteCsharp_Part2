5f817d5b14c16293daab00171588ca52f60a0b59
Cindy Morgan <camorgan70@gmail.com>
2021-12-27 19:18:26 -0500

    classes and objects

---
+++ UdemyCompleteCsharp17/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCompleteCsharp17
+{
+    public static class Car
+    {
+        static int speed;
+        static string color;
+        static float price;
+
+        public static void Start()
+        {
+            Console.WriteLine("Car started");
+        }
+
+        public static void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+
+        public static void Stop()
+        {
+            Console.WriteLine("Car stopped");
+        }
+    }
+}
