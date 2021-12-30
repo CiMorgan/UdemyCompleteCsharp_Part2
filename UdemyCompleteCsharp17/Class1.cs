d04c202d7b6788ebff26f5195735f49ed2f6aae6
Cindy Morgan <camorgan70@gmail.com>
2021-12-29 20:10:10 -0500

    inheritance - fail

--- UdemyCompleteCsharp17/Class1.cs
+++ UdemyCompleteCsharp17/Class1.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace UdemyCompleteCsharp17
 {
-    internal class Class1
+    public class Class1
     {
+        public void Print()
+        {
+            Console.WriteLine("Hello World");
+        }
     }
+    public class Class2 : Class1
+    {
+        Class2 obj = new Class2();
+        obj.Print();
+
+}
 }
